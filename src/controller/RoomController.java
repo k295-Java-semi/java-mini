583fde002471ddbe211a84b0b86a67055b3d3abb
j-hee-98 <wogml.hee@gmail.com>
2025-03-27 22:51:52 +0900

    modify room home

--- src/controller/RoomController.java
+++ src/controller/RoomController.java
@@ -1,7 +1,41 @@
 package controller;
 
+import model.dto.Room;
+
 public class RoomController {
-	public static void main(String[] args) {
+	
+	/**
+	 * 모든 방 정보 조회
+	 **/
+	public void getAllRooms() {
+		
+	}
+	
+	/**
+	 * 특정 방 정보 조회
+	 **/
+	public void getRoomById(int roomId) {
+		
+	}
+	
+	/**
+	 * 방 추가
+	 **/
+	public void addRoom(Room room, int userId) {
+		
+	}
+	
+	/**
+	 * 방 정보 수정
+	 **/
+	public void updateRoom(Room room, int userId) {
+		
+	}
+	
+	/**
+	 * 방 삭제
+	 **/
+	public void deleteRoom(int roomId, int userId) {
 		
 	}
 }
---
+++ src/model/dao/RoomDaoImpl.java
@@ -0,0 +1,35 @@
+package model.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.dto.Room;
+
+public class RoomDaoImpl implements RoomDao{
+
+	@Override
+	public List<Room> selectAllRooms() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addRoom(Room room, int userId) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoom(Room room, int userId) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRoom(int roomId, int userId) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
