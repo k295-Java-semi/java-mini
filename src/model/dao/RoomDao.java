81aa73479d7288a09ae99f44c8bea00ba92c3589
Hee <wogml.hee@gmail.com>
2025-03-27 17:26:21 +0900

    roomDao add method

--- src/model/dao/RoomDao.java
+++ src/model/dao/RoomDao.java
@@ -3,8 +3,30 @@ package model.dao;
 
 import model.dto.Room;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface RoomDao {
-    List<Room> selectAllRooms() throws Exception;
+	
+	/**
+	 * 전체 방 검색 메서드
+	 **/
+    List<Room> selectAllRooms() throws SQLException;
+    
+    /**
+	 * 새로운 방 추가하는 메서드 (create)
+	 **/
+    void addRoom(Room room, int userId) throws SQLException;
+    
+    /**
+	 * 방의 정보 수정하는 메서드 (update)
+	 **/
+    void updateRoom(Room room, int userId) throws SQLException;
+    
+    /**
+	 * 방 삭제 메서드 (delete)
+	 **/
+    void deleteRoom(int roomId, int userId) throws SQLException;
+    
+    
 }
